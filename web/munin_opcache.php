fe93ce58bb2740489a0a09578e15e1afa858c814
pumamd <pumamd@yandex.ru>
2016-01-22 15:13:53 +0200

    * graph fixes * ability to connect statistic from many php versions

--- web/munin_opcache.php
+++ web/munin_opcache.php
@@ -6,6 +6,7 @@
 header('Content-Type: text/plain');
 if(function_exists('opcache_get_status')) {
 	$data = opcache_get_status(false);
+	$data['php_version'] = 'php-'.(defined('PHP_VERSION')?PHP_VERSION:'???');
 	print json_encode($data);
 } else {
 	print json_encode(array());
